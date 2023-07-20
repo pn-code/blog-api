94671c85edabe4d6512e68aa3ea94c917a98fe19
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-20 14:12:07 -0700

    Created logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,17 @@
+const pino = require("pino");
+import dayjs from "dayjs";
+
+const logger = pino({
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true,
+        },
+    },
+    base: {
+        pid: false,
+    },
+    timestamp: () => `,"time":"${dayjs().format()}"`,
+});
+
+export default logger;
