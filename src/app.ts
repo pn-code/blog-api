0ef30fff3e556c837f0e60838801dd1984542073
Philip Nguyen <philipnguyen953@gmail.com>
2023-07-20 14:21:26 -0700

    Added logs, dbConnect, and routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import config from "config";
+import connect from "./utils/connect";
+import logger from "./utils/logger";
+import routes from "./routes";
+
+const port = config.get<number>("port");
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`Server is running on PORT ${3000}.`)
+app.listen(port, async () => {
+    logger.info(`Server is running at https://localhost:${3000}.`)
+    await connect();
+    routes(app);
 })
